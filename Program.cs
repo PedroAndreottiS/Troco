5f7fbe95e5b8d20226dc95d2bbfeeaa37d543536
PedroAndreottiS <pedroandreottijetforce@gmail.com>
2023-04-02 14:27:16 -0300

    ACABEI

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿Decimal ValorTotal, ValorPago, Troco;
+
+Console.Write("Quanto ficou? ");
+
+ValorTotal= Convert.ToDecimal(Console.ReadLine());
+
+
+
+
+Console.WriteLine("Valor recebido: ");
+
+
+
+
+
+ValorPago= Convert.ToDecimal(Console.ReadLine());
+
+
+
+
+
+Troco= ValorTotal - ValorPago;
+
+ Console.WriteLine($"Seu troco é...{Troco}");
+
+
+
+
+
+
+
